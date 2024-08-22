e50d5cfcab8db7dcf7afcc3ac121c48e022185c9
Qingzheng Gao <gaoqz.ph@gmail.com>
2024-08-22 11:34:25 +0800

    temporary solution

--- lua/simplify.lua
+++ lua/simplify.lua
@@ -8,17 +8,15 @@ end
 function F.func(input, env)
     local do_convert = env.engine.context:get_option('traditionalization')
     for cand in input:iter() do
-        if cand.text:find("⬚") then
-            local text = cand.text:gsub("⬚", "")
-
-            if do_convert then
-                yield(cand:to_shadow_candidate(cand.type, text, cand.comment))
+        if do_convert then
+            yield(cand)
+        else
+            local simplified_text = F.simplify:convert(cand.text)
+            if simplified_text == cand.text then
+                yield(cand)
             else
-                local simplified_text = F.simplify:convert(text)
                 yield(cand:to_shadow_candidate(cand.type, simplified_text, cand.comment))
             end
-        else
-            yield(cand)
         end
     end
 end
